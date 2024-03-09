709d0e247b0c9d74ec95c5d63c080a20cc117e4d
Clifford537 <cliffordmukosh@gmail.com>
2024-03-09 15:21:11 +0300

    print out

--- form.php
+++ form.php
@@ -1,11 +1,12 @@
 <?php 
 session_start();
-reguire_once('connction.php');
+
 
 $name =$_POST['name'];
 $email=$_POST["email"];
 $password=$_POST["pswd"];
 
-
-
+echo("$name <br>");
+echo("$email <br> ");   
+// echo "$password";
 ?>
